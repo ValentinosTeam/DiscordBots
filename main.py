a0e938f284715dae2f65457f53740f67de1a9fab
Alex_Joo <54570878+AlexJoo2003@users.noreply.github.com>
2024-12-27 07:19:32 +0100

    added uk bot

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ from dotenv import load_dotenv
 load_dotenv()
 scpEUbot = SCPBot(1271728138371465216, 1271728139726356494)
 scpEUbot.run(os.getenv("SCP_SL_EU_BOT_TOKEN"))
+scpUKbot = SCPBot(1271728138371465216, 1271728139726356494)
+scpUKbot.run(os.getenv("SCP_SL_UK_BOT_TOKEN"))
