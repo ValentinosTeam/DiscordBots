f03c0699045778049ae6720592b6575d63f9c623
Alexander Alnoumeh <alexanderalnoumeh@gmail.com>
2025-07-26 11:46:40 +0000

    added cedmod /list cleanup

---
+++ src/cedmodcleanupbot.py
@@ -0,0 +1,41 @@
+import discord
+from datetime import datetime
+
+class CleanupBot(discord.Client):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.messages = True
+        intents.message_content = True
+        intents.guilds = True
+        super().__init__(intents=intents)
+    
+    async def on_ready(self):
+        current_time = datetime.now().time()
+        formatted_time = current_time.strftime("%d/%m %H:%M:%S")
+        print(f"{current_time} - Logged on as {self.user}")
+
+    async def on_disconnect():
+        current_time = datetime.now().time()
+        formatted_time = current_time.strftime("%d/%m %H:%M:%S")
+        print(f"{current_time} - {self.user} disconnecting...")
+
+    async def on_close():
+        current_time = datetime.now().time()
+        formatted_time = current_time.strftime("%d/%m %H:%M:%S")
+        print(f"{current_time} - {self.user} shutting down...")
+
+    async def on_message(self, message):
+        if message.author != self.user:
+            return
+        channel = message.channel
+        messages = [msg async for msg in channel.history(limit=50)]
+        i = 0
+        for msg in messages[1:]:
+            if msg.author == self.user and msg.embeds[0]:
+                if "players online" in msg.embeds[0].description:
+                    i += 1
+                    await msg.delete()
+        if i > 0:
+            current_time = datetime.now().time()
+            formatted_time = current_time.strftime("%d/%m %H:%M:%S")
+            print(f"{current_time} - deleted {i} embeds.")
--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 from scpslbot import SCPBot
 from unturnedbot import UnturnedBot
+from cedmodcleanupbot import CleanupBot
 import os
 import asyncio
 
 async def run_bots():
-    unturned_bot = UnturnedBot(os.getenv("UNTURNED_API_TOKEN"))
+    #unturned_bot = UnturnedBot(os.getenv("UNTURNED_API_TOKEN"))
+    cleanup_botEU1 = CleanupBot()
+    cleanup_botEU2 = CleanupBot()
+    #cleanup_botUK1 = CleanupBot()
     await asyncio.gather(
-        unturned_bot.start(os.getenv("UNTURNED_BOT_TOKEN"))
+        #unturned_bot.start(os.getenv("UNTURNED_BOT_TOKEN"))
+        cleanup_botEU1.start(os.getenv("SCP_SL_EU_BOT_TOKEN")),
+        cleanup_botEU2.start(os.getenv("SCP_SL_EU_BOT_2_TOKEN")),
+        #cleanup_botUK1.start(os.getenv("SCP_SL_UK_BOT_TOKEN"))
     )
 
 if __name__ == "__main__":
