ff8d2a223938519fafc4927fa66f5690095b6b0e
Alex_Joo <54570878+AlexJoo2003@users.noreply.github.com>
2025-08-02 21:26:20 +0200

    added donator and vcmover to main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from cedmodcleanupbot import CleanupBot
 from reactbot import ReactBot
 from donatormanagmentbot import DonatorManagmentBot
+from tempvcmover import VcMover
 import os
 import asyncio
 
@@ -9,10 +10,13 @@ async def run_bots():
     cleanup_botEU2 = CleanupBot()
     react_bot = ReactBot([1209138626152103946, 1234146125326454877])
     donator_bot = DonatorManagmentBot(1208219396791730226, 1216884748077498418, [1266685217578422333, 1249485247372853278, 1248751562101231657], [1209949259076739184], 1400529250204909660, [1221906406035423292, 1248721750095564871, 1248721839212204152, 1248722038336651315, 1208569460856848447])
+    vc_mover_bot = VcMover(1249492439195193375, 1208219397794177055)
     await asyncio.gather(
         cleanup_botEU1.start(os.getenv("SCP_SL_EU_BOT_TOKEN")),
         cleanup_botEU2.start(os.getenv("SCP_SL_EU_BOT_2_TOKEN")),
-        react_bot.start(os.getenv("SCP_SL_EU_BOT_TOKEN"))
+        react_bot.start(os.getenv("SCP_SL_EU_BOT_TOKEN")),
+        donator_bot.start(os.getenv("SCP_SL_EU_BOT_TOKEN")),
+        vc_mover_bot.start(os.getenv("SCP_SL_EU_BOT_TOKEN"))
     )
 
 if __name__ == "__main__":
